0105361b755b34ed49bca72e40960a1cd7ddb6dd
Eric Barton <brumpo@Erics-MacBook-Pro.local>
2022-06-30 14:37:02 -0400

    fixed product routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     product = Product.all
     render json: product
   end
+
+  def first_product
+    product = Product.first
+    render json: Product.first
+  end
+
+  def second_product
+    product = Product.second
+    render json: Product.second
+  end
+
+  def third_product
+    product = Product.third
+    render json: Product.third
+  end
 end
